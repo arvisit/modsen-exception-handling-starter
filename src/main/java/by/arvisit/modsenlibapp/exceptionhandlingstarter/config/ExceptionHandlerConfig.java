040be588acfda09a282c5009e9dd0aaf12fe8d39
Artem Sitko <sitko.ar@gmail.com>
2024-02-07 03:34:43 +0300

    Add config classes

---
+++ src/main/java/by/arvisit/modsenlibapp/exceptionhandlingstarter/config/ExceptionHandlerConfig.java
@@ -0,0 +1,20 @@
+package by.arvisit.modsenlibapp.exceptionhandlingstarter.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import by.arvisit.modsenlibapp.exceptionhandlingstarter.handler.GlobalExceptionHandlerAdvice;
+
+@Configuration
+@ConditionalOnProperty(prefix = "exception.handling", name = "include", havingValue = "true")
+public class ExceptionHandlerConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    GlobalExceptionHandlerAdvice globalHandlerAdvice() {
+        return new GlobalExceptionHandlerAdvice();
+    }
+    
+}
