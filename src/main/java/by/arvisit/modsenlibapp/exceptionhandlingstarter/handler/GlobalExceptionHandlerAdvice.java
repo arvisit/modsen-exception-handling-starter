6ff71885e327ecc31635b814b57e66e6c41203dd
Artem Sitko <sitko.ar@gmail.com>
2024-02-07 03:35:27 +0300

    Add global exception handler class

---
+++ src/main/java/by/arvisit/modsenlibapp/exceptionhandlingstarter/handler/GlobalExceptionHandlerAdvice.java
@@ -0,0 +1,97 @@
+package by.arvisit.modsenlibapp.exceptionhandlingstarter.handler;
+
+import java.net.ConnectException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import by.arvisit.modsenlibapp.exceptionhandlingstarter.exception.ExceptionResponse;
+import by.arvisit.modsenlibapp.exceptionhandlingstarter.exception.MultiExceptionResponse;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandlerAdvice {
+
+    private static final ZoneId EUROPE_MINSK_TIMEZONE = ZoneId.of("Europe/Minsk");
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ExceptionResponse handle(EntityNotFoundException exception) {
+        return ExceptionResponse.builder()
+                .withStatus(HttpStatus.NOT_FOUND.value())
+                .withMessage(exception.getMessage())
+                .withTimeStamp(ZonedDateTime.now(EUROPE_MINSK_TIMEZONE))
+                .build();
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ExceptionResponse handle(HttpRequestMethodNotSupportedException exception) {
+        return ExceptionResponse.builder()
+                .withStatus(HttpStatus.METHOD_NOT_ALLOWED.value())
+                .withMessage(exception.getMessage())
+                .withTimeStamp(ZonedDateTime.now(EUROPE_MINSK_TIMEZONE))
+                .build();
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handle(HttpMessageNotReadableException exception) {
+        return ExceptionResponse.builder()
+                .withStatus(HttpStatus.BAD_REQUEST.value())
+                .withMessage(exception.getMessage())
+                .withTimeStamp(ZonedDateTime.now(EUROPE_MINSK_TIMEZONE))
+                .build();
+    }
+
+    @ExceptionHandler(ConnectException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionResponse handle(ConnectException exception) {
+        return ExceptionResponse.builder()
+                .withStatus(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .withMessage(exception.getMessage())
+                .withTimeStamp(ZonedDateTime.now(EUROPE_MINSK_TIMEZONE))
+                .build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MultiExceptionResponse handle(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors()
+                .forEach(error -> {
+                    String fieldName = error.getField();
+                    String errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+        return MultiExceptionResponse.builder()
+                .withStatus(HttpStatus.BAD_REQUEST.value())
+                .withMessages(errors)
+                .withTimeStamp(ZonedDateTime.now(EUROPE_MINSK_TIMEZONE))
+                .build();
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MultiExceptionResponse handle(ConstraintViolationException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getConstraintViolations()
+                .forEach(constraintViolation -> constraintViolation.getPropertyPath()
+                        .forEach(error -> errors.put(error.getName(), constraintViolation.getMessage())));
+        return MultiExceptionResponse.builder()
+                .withStatus(HttpStatus.BAD_REQUEST.value())
+                .withMessages(errors)
+                .withTimeStamp(ZonedDateTime.now(EUROPE_MINSK_TIMEZONE))
+                .build();
+    }
+}
